dcc95a576dbd6ce6b73017f02484b9aa83787325
Dani Baumann <git@danibaumann.ch>
2021-02-11 21:01:56 +0100

    added route for wirkleistung only

--- src/controllers/stats.js
+++ src/controllers/stats.js
@@ -45,4 +45,43 @@ const getStats = asyncHandler(async (req, res, next) => {
   res.status(200).json(dataToReturn)
 })
 
-export { getStats }
+const getSmlStats = asyncHandler(async (req, res, next) => {
+  const hosts = process.env.HOSTS.split(',')
+  log.debug(hosts)
+
+  if (!hosts || hosts.length === 0) {
+    return res.status(500).json('No Hosts provided in environment')
+  }
+
+  log.info('will fetch latest data')
+  const dataToReturn = []
+  for await (const h of hosts) {
+    log.debug(`Will query host: ${h}`)
+    let i = 0
+    const data = {}
+    for await (const v of EmuStat) {
+      if (v.id === 'AktuelleWirkLeistungTotal') {
+        const id = v.id
+        log.debug(`Fetching Data: ${id}`)
+        try {
+        } catch (err) {
+          log.error(err.message)
+        }
+        const response = await axios.get(`http://${h}/${v.register}`)
+        data[id] = { value: response.data, unit: v.unit, label: v.label }
+        // tmpArray.push({
+        //   id:,
+        // })
+        i++
+      } else {
+        return
+      }
+    }
+    dataToReturn.push({ ip: h, data })
+  }
+
+  log.debug(dataToReturn)
+  res.status(200).json(dataToReturn)
+})
+
+export { getStats, getSmlStats }
--- src/routes/statesRoute.js
+++ src/routes/statesRoute.js
@@ -1,7 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { getStats } from '../controllers/stats.js'
+import { getStats, getSmlStats } from '../controllers/stats.js'
 
 router.get('/', getStats)
+
+router.get('/wirkleistung', getSmlStats)
 
 export default router
